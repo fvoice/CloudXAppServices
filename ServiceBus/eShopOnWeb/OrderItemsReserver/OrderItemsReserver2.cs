7a8b032a3e05a2ccc69f93f3c18826831cf4b627
fvoice <fvoice@yandex.ru>
2022-02-19 12:05:44 +0100

    sending an email

--- ServiceBus/eShopOnWeb/OrderItemsReserver/OrderItemsReserver2.cs
+++ ServiceBus/eShopOnWeb/OrderItemsReserver/OrderItemsReserver2.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs;
@@ -19,24 +20,36 @@ namespace OrderItemsReserver
             var logger = context.GetLogger("OrderItemsReserver2");
             logger.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
 
-            logger.LogInformation("Processing an order");
+			try
+			{
+				logger.LogInformation("Processing an order");
 
-            //var content = await new StreamReader(req.Body).ReadToEndAsync();
+				//var content = await new StreamReader(req.Body).ReadToEndAsync();
 
-            Order order = JsonConvert.DeserializeObject<Order>(myQueueItem);
+				Order order = JsonConvert.DeserializeObject<Order>(myQueueItem);
 
-            logger.LogInformation("The order details:");
-            logger.LogInformation(myQueueItem);
+				logger.LogInformation("The order details:");
+				logger.LogInformation(myQueueItem);
 
-            BlobServiceClient blobServiceClient = new BlobServiceClient(
-                "DefaultEndpointsProtocol=https;AccountName=storageaccounttorema94f;AccountKey=1ehBBieUWsNtOjGX1FN6XQEujQRAB0DTTF4lssuSbzIrRDwa+W4sHQEISK7EPduYBfKiLF7Xb5ocgzHb0fGApQ==;EndpointSuffix=core.windows.net");
-            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("orders");
-            await containerClient.CreateIfNotExistsAsync();
+				BlobServiceClient blobServiceClient = new BlobServiceClient(
+					"DefaultEndpointsProtocol=https;AccountName=storageaccounttorema94f;AccountKey=1ehBBieUWsNtOjGX1FN6XQEujQRAB0DTTF4lssuSbzIrRDwa+W4sHQEISK7EPduYBfKiLF7Xb5ocgzHb0fGApQ==;EndpointSuffix=core.windows.net");
+				BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("orders");
+				await containerClient.CreateIfNotExistsAsync();
 
-            logger.LogInformation("Uploading to Blob container starting");
-            BlobClient blobClient = containerClient.GetBlobClient($"{Guid.NewGuid().ToString()}.json");
-            await blobClient.UploadAsync(new MemoryStream(Encoding.UTF8.GetBytes(myQueueItem)));
-            logger.LogInformation("Uploading to Blob container finished");
+				logger.LogInformation("Uploading to Blob container starting");
+				BlobClient blobClient = containerClient.GetBlobClient($"{Guid.NewGuid().ToString()}.json");
+				await blobClient.UploadAsync(new MemoryStream(Encoding.UTF8.GetBytes(myQueueItem)));
+				logger.LogInformation("Uploading to Blob container finished");
+			}
+			catch (Exception e)
+			{
+				using (var client = new HttpClient())
+				{
+					await client.PostAsync("https://fvoicesendmail.azurewebsites.net:443/api/sendmail/triggers/manual/invoke?api-version=2020-05-01-preview&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=FAgVKDHpECqyP0CPu1r-cj1hG8GcckTDBEVOsusNqCE", 
+						new StringContent(e.ToString()));
+				}
+				throw;
+			}
         }
     }
 }
